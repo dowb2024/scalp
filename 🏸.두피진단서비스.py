1aa4ed542fe580d7fbec483ec653a05ce3b84015
dowb2024 <dowb1004@gmail.com>
2024-11-07 17:29:18 +0900

    Update 🏸.두피진단서비스.py

--- 🏸.두피진단서비스.py
+++ 🏸.두피진단서비스.py
@@ -950,14 +950,14 @@ elif st.session_state.page == 1:
     st.markdown("**🔥 설문조사**")
     st.text("* 당신의 두피에 대해 알려주세요. 더 정확한 분석에 도움이 됩니다.")
     st.text("* 질문 선택 후 Submit 버튼을 반드시 눌러주세요.")
-    # auto_complete = st.toggle("예시 데이터로 채우기")
+    auto_complete = st.toggle("예시 데이터로 채우기")
     with (st.form(key="form")):
 
         type = st.multiselect(
             label="◾ 질문 1. 당신의 두피 타입을 선택하세요",
             options=list(type_emoji_dict.keys()),
             max_selections=1,
-            default=scalp_example["type"] if auto_complete else []
+            default=scalp_example["type"] if auto_complete else []            
         )
 
         symptom = st.multiselect(
